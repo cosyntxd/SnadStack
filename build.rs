2f87744d49bc115bfc260079006fb18e37557de8
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-11-28 22:33:21 -0800

    config defined cell properties
    dont need to have multiple long match statements inside of the code

---
+++ build.rs
@@ -0,0 +1,89 @@
+use std::env;
+use std::fmt::format;
+use std::fs;
+use std::path::Path;
+
+fn parse_hex(hex: &str) -> Result<[u8; 3], &'static str> {
+    if hex.len() != 7 {
+        return Err("Hex color code must be 6 characters long");
+    }
+    if &hex[0..1] != "#" {
+        return Err("needs # symbol at sttart of hex");
+    }
+    let r = u8::from_str_radix(&hex[1..3], 16).map_err(|_| "Invalid red component")?;
+    let g = u8::from_str_radix(&hex[3..5], 16).map_err(|_| "Invalid green component")?;
+    let b = u8::from_str_radix(&hex[5..7], 16).map_err(|_| "Invalid blue component")?;
+
+    Ok([r, g, b])
+}
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("cell_settings.rs");
+
+    let enum_definitions =
+        fs::read_to_string("assets/cell-definitions.csv").expect("Failed to read settings file");
+    let mut parsed_lines = vec![];
+    for line in enum_definitions.lines() {
+        let parts: Vec<_> = line.split(' ').collect();
+
+        let (i, rest) = parts[0].split_at(1);
+        let name = format!("{}{}", i.to_uppercase(), rest);
+
+        let character = parts[1];
+        let rgb_start = parse_hex(parts[2]).unwrap();
+        let rgb_end = parse_hex(parts[3]).unwrap();
+
+        parsed_lines.push((name, character, rgb_start, rgb_end))
+    }
+
+    let length = parsed_lines.len();
+
+    let names = parsed_lines
+        .iter()
+        .map(|(name, _, _, _)| format!("{name},"))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let colors = parsed_lines
+        .iter()
+        .map(|(_, _, start, end)| format!("[{start:?}, {end:?}]"))
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let inputs = parsed_lines
+        .iter()
+        .map(|(name, character, _, _)| {
+            format!("VirtualKeyCode::{character} => *self = CellType::{name}")
+        })
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let definition = format!(
+        "
+        use winit::event::VirtualKeyCode;
+        const COLOR_LOOKUP: [[[u8;3];2];{length}] = [{colors}];
+        #[repr(usize)]
+        #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+        pub enum CellType {{
+            {names}
+        }}
+        impl CellType {{
+            pub fn color(&self) -> &[[u8;3];2]  {{
+                unsafe {{
+                    &COLOR_LOOKUP[(*self as usize)]
+                }}
+            }}
+            pub fn switch_if_valid(&mut self, code: VirtualKeyCode) {{
+                match code {{
+                    {inputs},
+                    _ => ()
+                }}         
+            }}
+        }}
+        "
+    );
+
+    fs::write(&dest_path, definition).unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=assets/cell-definitions.csv");
+}
--- src/cells.rs
+++ src/cells.rs
@@ -1,55 +1,55 @@
+include!(concat!(env!("OUT_DIR"), "/cell_settings.rs"));
+
+// #[repr(packed)]
 #[derive(Clone, Copy)]
 pub struct Cell {
     pub material: CellType,
     pub rgb: [u8; 3],
     pub selected: bool,
-    pub updated: u16,
+    pub updated: u8,
+    pub discolored: bool,
+    pub health: u16,
+    pub lifespan: u16, // particles
+                       // pub velocity_x: i16,
+                       // pub velocity_y: i16,
+                       // pub position_x: i16,
+                       // pub position_y: i16,
 }
-impl Default for Cell {
-    fn default() -> Self {
-        Self {
-            material: CellType::Air,
-            rgb: Self::rgb_ranges(CellType::Air),
-            selected: false,
-            updated: 0,
-        }
-    }
-}
-
 impl Cell {
     pub fn new(material: CellType) -> Self {
         let rgb = Self::rgb_ranges(material);
         Self {
             material,
             rgb,
-            ..Default::default()
+            selected: false,
+            updated: 0,
+            // velocity_x: 0,
+            // velocity_y: 0,
+            health: 0,
+            discolored: false,
+            lifespan: 0,
+            // position_x: 0,
+            // position_y: 0,
         }
     }
-    pub fn rgb_ranges(material: CellType) -> ([u8; 3]) {
+
+    pub fn rgb_ranges(material: CellType) -> [u8; 3] {
         let random_variance = fastrand::i16(0..=100);
 
-        let (rgb_start, rgb_end) = match material {
-            CellType::Air => ([125, 201, 255], [125, 201, 255]),
-            CellType::Water => ([76, 153, 243], [104, 175, 253]),
-            CellType::Sand => ([220, 180, 116], [204, 164, 100]),
-            CellType::Stone => ([131, 143, 134], [110, 122, 113]),
-            _ => ([255, 155, 61], [117, 36, 81]),
-        };
+        let [rgb_start, rgb_end] = material.color();
 
         let mut rgb = [0, 0, 0];
         rgb.iter_mut().enumerate().for_each(|(index, value)| {
-            let rgba_diff = rgb_end[index] - rgb_start[index];
-            let rgba_change = rgba_diff * random_variance / 100;
-            *value = (rgb_start[index] + rgba_change) as u8;
+            let rgba_diff = rgb_end[index] as i16 - rgb_start[index] as i16;
+            let rgba_change = rgba_diff as i16 * random_variance / 100;
+            *value = (rgb_start[index] as i16 + rgba_change as i16) as u8;
         });
         rgb
     }
-}
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum CellType {
-    None,
-    Air,
-    Water,
-    Sand,
-    Stone,
+    pub fn cfg_switch(&mut self, bool: bool) {
+        *self = match bool {
+            _ => Self::new(CellType::Air),
+            _ => *self,
+        };
+    }
 }
