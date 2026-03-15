a7927809e5643a6fcc7e70cc845f19030125f5e2
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2026-03-14 23:43:12 -0700

    wsl \r\n fix

--- run.sh
+++
@@ -1,2 +0,0 @@
-cargo build --target x86_64-pc-windows-gnu --release
-cmd.exe /C target\\x86_64-pc-windows-gnu\\release\\sand-sim.exe
---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+cargo build --target x86_64-pc-windows-gnu --release && cmd.exe /c "target\x86_64-pc-windows-gnu\release\sand-sim.exe"
